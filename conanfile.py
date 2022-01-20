f7ecb39f71b8d34bfda2b9af0daef15fc391d36e
Valerij Bielskij <lunacik18@gmail.com>
2022-01-20 17:29:59 +0200

    Improved dependency management with conan

--- conanfile.py
+++ conanfile.py
@@ -12,12 +12,14 @@ class MosaicDecomposerProject(ConanFile):
     requires = (
         "catch2/2.13.7",
         "docopt.cpp/0.6.2",
-        "spdlog/1.9.2",
+        "spdlog/1.9.2"
     )
     generators = "cmake", "gcc", "txt", "cmake_find_package"
 
     def requirements(self):
         if self.options.use_conan_opencv == "ON":
             self.requires("opencv/4.0.1@conan/stable")
+            self.requires("bzip2/1.0.8")
+            self.requires("libjpeg/9c")
 
 
